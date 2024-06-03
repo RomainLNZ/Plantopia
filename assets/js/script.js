510dbeb70a9bfe3a9ef6d1c481b83c6977c751ff
Djason16 <djason1607@gmail.com>
2024-06-03 15:34:51 +0200

    Version final responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,52 @@
-function checkKeyPress(event) {
-    if (event.key === "Enter") {
-        performsearch();
-    }
+// Logos start
+document.querySelector('.logoDesktop').addEventListener('click', function () {
+    window.location.href = 'home.html';
+});
+
+document.querySelector('.logoMobile').addEventListener('click', function () {
+    window.location.href = 'home.html';
+});
+// Logos end
+
+//Mobile navbar start
+var hiddenNav = document.getElementById('hiddenNav');
+var navList = document.getElementById("navList");
+var searchBar = document.getElementById('searchMobileBar');
+
+console.log(hiddenNav);
+console.log(navList);
+
+if (hiddenNav) {
+    hiddenNav.addEventListener('click', function () {
+        this.classList.toggle('close');
+        toggleNav();
+        toggleSearchBar();
+    });
 }
 
-function performsearch() {
-    var searchInput = document.getElementById("searchinput").value.trim();
+function toggleNav() {
+    navList.classList.toggle("showNav");
+}
+
+function toggleSearchBar() {
+    searchBar.style.display = 'none';
+}
+//Mobile navbar end
+
+//Search start
+//  Desktop start
+document.getElementById('searchInput').addEventListener('keydown', function (event) {
+    if (event.key === "Enter") {
+        performSearch();
+    }
+});
+
+document.querySelector('.performSearch').addEventListener('click', function () {
+    performSearch();
+});
+
+function performSearch() {
+    var searchInput = document.getElementById("searchInput").value.trim();
 
     if (searchInput !== "") {
         alert("Search will be performed for: " + searchInput);
@@ -13,17 +54,17 @@ function performsearch() {
         alert("Please enter a search query.");
     }
 }
+//  Desktop end
 
-document.addEventListener('DOMContentLoaded', function () {
-    var hiddennav = document.getElementById('hiddennav');
-    if (hiddennav) {
-        hiddennav.addEventListener('click', function () {
-            this.classList.toggle('close');
-        });
+//  Mobile start
+document.getElementById('searchMobileIcon').addEventListener('click', function () {
+    var searchBar = document.getElementById('searchMobileBar');
+
+    if (searchBar.style.display === 'none' || searchBar.style.display === '') {
+        searchBar.style.display = 'block';
+    } else {
+        searchBar.style.display = 'none';
     }
 });
-
-function toggleNav() {
-    var navList = document.getElementById("navlist");
-    navList.classList.toggle("show-nav");
-}
+//  Mobile end
+//Search end
